e33298cb03400ff430e1ee0142d05e04ceb8e0e1
BrayanDaniloFlorezEsparzaADSO <brayandanilo2004@gmail.com>
2022-11-15 09:38:44 -0500

    6

---
+++ mian.js
@@ -0,0 +1,23 @@
+addEventListener("DOMContentLoaded", (e)=>{
+    let form=document.querySelector('#input')
+
+    let cont =1   
+    form.addEventListener("submit",(e)=>{
+        e.preventDefault();       
+
+        if(cont <=50){
+        let datosEntrada = Object.fromEntries(new FormData(e.target))
+
+            let observaciones= datosEntrada.observaciones
+            let tabla=document.querySelector("tbody");
+            tabla.insertAdjacentHTML("beforeend",`
+                <tr>
+                    <td> ${cont}  </td>
+                    <td> ${observaciones}  </td>
+                </tr>
+                `)
+           
+            cont++
+        }
+    })               
+})
